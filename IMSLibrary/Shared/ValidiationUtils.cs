8e30d9557ed6f0b7987cdcc89b209870019e64c7
reece <consolelogreece@gmail.com>
2018-09-05 16:29:16 +0100

    Added utilities for valdiation

---
+++ IMSLibrary/Shared/ValidiationUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSLibrary.Shared
+{
+    public static class ValidiationUtils
+    {
+        /// <summary>
+        /// Check if string is valid decimal type.
+        /// </summary>
+        /// <param name="val">String to check</param>
+        /// <returns>True/False depending if valid decimal</returns>
+        public static bool IsValidDecimal(string val)
+        {
+            return Decimal.TryParse(val, out var x);
+        }
+
+        /// <summary>
+        /// Check if string is valid int type.
+        /// </summary>
+        /// <param name="val">String to check</param>
+        /// <returns>True/False depending if valid int</returns>
+        public static bool IsValidInt(string val)
+        {
+            return int.TryParse(val, out var x);        
+        }
+
+        /// <summary>
+        /// Check if string is valid DateTime type.
+        /// </summary>
+        /// <param name="val">String to check</param>
+        /// <returns>True/False depending if valid DateTime</returns>
+        public static bool IsValidDate(string val)
+        {
+            return DateTime.TryParse(val, out var x);
+        }
+    }
+}
